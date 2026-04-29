4fc3fb75d6029bc30bd8e0aecb3dfbb5926f7ffa
hbc <me@hbc.rocks>
2026-04-29 12:04:24 -0700

    Use Kubernetes duration formatting in CLI

--- cmd/kubectl-unbounded/app/format.go
+++ cmd/kubectl-unbounded/app/format.go
@@ -4,23 +4,11 @@
 package app
 
 import (
-	"fmt"
 	"time"
+
+	"k8s.io/apimachinery/pkg/util/duration"
 )
 
-// formatAge formats elapsed time since t as a human-readable duration
-// (e.g. "5m", "2h", "3d").
 func formatAge(t time.Time) string {
-	d := time.Since(t)
-
-	switch {
-	case d < time.Minute:
-		return fmt.Sprintf("%ds", int(d.Seconds()))
-	case d < time.Hour:
-		return fmt.Sprintf("%dm", int(d.Minutes()))
-	case d < 24*time.Hour:
-		return fmt.Sprintf("%dh", int(d.Hours()))
-	default:
-		return fmt.Sprintf("%dd", int(d.Hours()/24))
-	}
+	return duration.HumanDuration(time.Since(t))
 }
